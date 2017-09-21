543a56f259435375cba9969fa32d82c479f1c602
Trevor <tsiu086@uottawa.ca>
2017-09-21 16:22:57 -0400

    first commit

---
+++ Design2.java
@@ -0,0 +1,51 @@
+
+public class Design2 extends Design5 {
+	  
+	  public Design2(char type, double xOrRho, double yOrTheta)
+	  {
+	    if(type == 'C'){
+	    	this.xOrRho= Math.sqrt(Math.pow(xOrRho, 2) + Math.pow(yOrTheta, 2));
+	    	this.yOrTheta= Math.toDegrees(Math.atan2(yOrTheta, xOrRho));
+	    }
+	    else if ( type == 'P'){
+		    this.xOrRho = xOrRho;
+		    this.yOrTheta = yOrTheta;
+	    }
+		else{
+			throw new IllegalArgumentException();
+		}
+		    	
+	    typeCoord = type;
+	  }
+	  public double getX()
+	  { 
+	       return (Math.cos(Math.toRadians(yOrTheta)) * xOrRho);
+	  }
+	  
+	  public double getY()
+	  { 
+	      
+	      return (Math.sin(Math.toRadians(yOrTheta)) * xOrRho);
+	  }
+	  
+	  public double getRho()
+	  {
+	      return xOrRho;
+	   
+	  }
+	  
+	  public double getTheta()
+	  {
+	      return yOrTheta;
+	    
+	  }
+	  
+		
+	  /**
+	   * Converts Cartesian coordinates to Polar coordinates.
+	   */
+	  
+	 
+	  
+
+}
