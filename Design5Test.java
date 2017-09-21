0b6a9624edc8772f3be8df11babe463f4edefab6
mikemd833 <mdiep044@uottawa.ca>
2017-09-21 16:56:31 -0400

    Updated

--- Design5Test.java
+++ Design5Test.java
@@ -19,7 +19,7 @@ public class Design5Test {
 	   */
 	  public static void main(String[] args)
 	  {
-	    Design5 point;
+	    Design3 point;
 
 	    System.out.println("Cartesian-Polar Coordinates Conversion Program");
 
@@ -28,7 +28,7 @@ public class Design5Test {
 	    // If he did not, prompt the user for them.
 	    try
 	    {
-	      point = new Design5(args[0].toUpperCase().charAt(0), 
+	      point = new Design3(args[0].toUpperCase().charAt(0), 
 	        Double.valueOf(args[1]).doubleValue(), 
 	        Double.valueOf(args[2]).doubleValue());
 	    }
@@ -66,7 +66,7 @@ public class Design5Test {
 	   * @throws IOException If there is an error getting input from
 	   *         the user.
 	   */
-	  private static Design5 getInput() throws IOException
+	  private static Design3 getInput() throws IOException
 	  {
 	    byte[] buffer = new byte[1024];  //Buffer to hold byte input
 	    boolean isOK = false;  // Flag set if input correct
@@ -144,7 +144,7 @@ public class Design5Test {
 	      isOK = false;
 	    }
 	    //Return a new PointCP object
-	    return (new Design5 (coordType, a, b));
+	    return (new Design3 (coordType, a, b));
 	  }
 }
 
