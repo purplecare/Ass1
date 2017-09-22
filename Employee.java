e78146ce1ff2c229937913e0c5b8a75dfd74a77f
mikemd833 <mdiep044@uottawa.ca>
2017-09-22 18:35:37 -0400

    Stuck on adding an address, please help

--- Employee.java
+++ Employee.java
@@ -2,10 +2,15 @@ public class Employee {
 	private String name;
 	private int hr;
 	private double rate;
-	private String Address
-	public Employee(String name, int hr, double rate, String address)
+	private Address[] addressList = new Address[5];
+	public Employee(String name, int hr, double rate, Address[] addressList){
 		this.name = name;
 		this.hr = hr;
 		this.rate = rate;
-		this.address = address;
+		this.addressList = addressList;
+	}
+	public static void main(String[] args) {
+		Employee o1 = new Employee("Falcao", 40, 15.50, addressList);
+	}
+	//new Address("Queen",48,"K1P1N2"),new Address("King Edward",800,"K1N6N5")
 }
