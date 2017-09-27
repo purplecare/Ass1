05dc6d4d289f2c3537887b4f182fb4959284a4f5
mikemd833 <mdiep044@uottawa.ca>
2017-09-27 19:24:00 -0400

    Formatted Code, and Edited

--- Design3.java
+++ Design3.java
@@ -1,53 +1,35 @@
 
 public class Design3 extends Design5 {
 	
-	  
-	  public Design3(char type, double xOrRho, double yOrTheta)
-	  {
-	    if(type == 'C'){
-	    	this.xOrRho= xOrRho;
-	    	this.yOrTheta= yOrTheta;
-	    }
-	    else if ( type == 'P'){
+	public Design3(char type, double xOrRho, double yOrTheta) {
+		if(type == 'C') {
+			this.xOrRho = xOrRho;
+			this.yOrTheta = yOrTheta;
+		} else if ( type == 'P') {
 		    this.xOrRho = (Math.cos(Math.toRadians(yOrTheta)) * xOrRho);
 		    this.yOrTheta = (Math.sin(Math.toRadians(yOrTheta)) * xOrRho);
-	    }
-		else{
+		} else {
 			throw new IllegalArgumentException();
 		}
 		    	
-	    typeCoord = type;
-	  }
-	  public double getX()
-	  { 
-	       return  xOrRho;
-	  }
+		typeCoord = type;
+	}
+	
+	 //Instance methods **************************************************
+	public double getX() { 
+		return  xOrRho;
+	}
 	  
-	  public double getY()
-	  { 
-	      
-	      return yOrTheta ;
-	  }
+	public double getY() {    
+		return yOrTheta ;
+	}
 	  
-	  public double getRho()
-	  {
-	      return Math.sqrt(Math.pow(xOrRho, 2) + Math.pow(yOrTheta, 2));
-	   
-	  }
+	public double getRho() {
+		return Math.sqrt(Math.pow(xOrRho, 2) + Math.pow(yOrTheta, 2));
+	}
 	  
-	  public double getTheta()
-	  {
-	      return Math.toDegrees(Math.atan2(yOrTheta, xOrRho));
-	    
-	  }
-	  
-		
-	  /**
-	   * Converts Cartesian coordinates to Polar coordinates.
-	   */
-	  
-	 
-	  
-
+	public double getTheta() {
+		return Math.toDegrees(Math.atan2(yOrTheta, xOrRho));
+	}
 
 }
