f666c7855f1f0d53f4bc51ad5b8c0f4a80a27387
mikemd833 <mdiep044@uottawa.ca>
2017-09-27 14:12:56 -0400

    Updated Mike

--- Employee.java
+++ Employee.java
@@ -1,20 +1,4 @@
 public class Employee {
-<<<<<<< HEAD
-	private String name;
-	private int hr;
-	private double rate;
-	private Address[] addressList = new Address[5];
-	public Employee(String name, int hr, double rate, Address[] addressList){
-		this.name = name;
-		this.hr = hr;
-		this.rate = rate;
-		this.addressList = addressList;
-	}
-	public static void main(String[] args) {
-		Employee o1 = new Employee("Falcao", 40, 15.50, addressList);
-	}
-	//new Address("Queen",48,"K1P1N2"),new Address("King Edward",800,"K1N6N5")
-=======
 	private  String name;
 	private int hr;
 	private double rate;
@@ -28,8 +12,6 @@ public class Employee {
 		
 	}
 	public String toString(){
-		return this.name+this.hr+this.rate+this.address_list;
+		return "Name: "+this.name +" Hours: "+ this.hr+ " Rate: "+this.rate+" Address: "+this.address_list;
 	}
-
->>>>>>> 2b9d6c16ff983ec468a4c067e9f15d5bf1f03adf
 }
